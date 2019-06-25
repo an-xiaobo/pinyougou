5133df24c37b00344fea35070e55b7b0f4ca22ba
itcast <bos@itcast.cn>
2019-06-25 21:42:22 +0800

    tiajia

---
+++ pinyougou_parent/xiaobo_test/src/main/java/test.java
@@ -0,0 +1,13 @@
+/**
+ * @author xiaobo
+ * @package PACKAGE_NAME
+ * @time 2019/6/25 0025 12:23
+ * @Version: 1.0
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("test");
+        System.out.println("test1");
+        System.out.println("test2");
+    }
+}
