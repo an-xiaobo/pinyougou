9de17d2abdff2818de2fc2a31a82bbe6342d38bd
xiaobo <954212654@qq.com>
2019-06-25 12:24:59 +0800

    测试提交2-xiaobo

---
+++ pinyougou_parent/xiaobo_test/src/main/java/test.java
@@ -0,0 +1,11 @@
+/**
+ * @author xiaobo
+ * @package PACKAGE_NAME
+ * @time 2019/6/25 0025 12:23
+ * @Version: 1.0
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
