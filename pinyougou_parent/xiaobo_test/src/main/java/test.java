ef58e5f2b830d8f0ad05e7b1cf2e9adf002cc38c
xiaobo <954212654@qq.com>
2019-06-25 15:29:22 +0800

    xiaobo

--- pinyougou_parent/xiaobo_test/src/main/java/test.java
+++ pinyougou_parent/xiaobo_test/src/main/java/test.java
@@ -10,5 +10,6 @@ public class test {
         System.out.println("test1");
         System.out.println("test2");
         System.out.println("test5");
+        System.out.println("test6");
     }
 }
